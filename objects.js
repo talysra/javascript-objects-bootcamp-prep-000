cc02fe691d500e7a960b9ec562f33db9e66e03cc
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-02 18:06:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,4 @@ const playlist = {
   Korn: "Blind"
 };
 
-function updatePlaylist(obj, key, value) {
-
-  return Object.assign({}, obj, { [key]: value })
-}
-
 updatePlaylist(playlist, 'Rammstein', 'Adios')
